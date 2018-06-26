51d0cf0834000f8f524c56b128b1f669fc0778f9
HONG <HONG@DESKTOP-35SRCQJ>
2018-06-26 18:22:21 +0900

    Interest 추가

--- src/com/libo/libo/member/service/IntersetService.java
+++ src/com/libo/libo/member/service/IntersetService.java
@@ -1,5 +1,19 @@
 package com.libo.libo.member.service;
 
+import java.util.List;
+
+import com.libo.libo.member.Interset;
+
 public class IntersetService {
+
+	// 관심사 등록
+	public void insertInterset(Interset interset) {
+
+	}
+
+	// 관심사 목록
+	public List<Interset> getIntersetList(String memberId) {
+		return null;
+	}
 
 }
