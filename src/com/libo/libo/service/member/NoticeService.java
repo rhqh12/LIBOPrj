97b3df8e4e44a06fb737eb662b1173d43f4244f3
user <user@user-pc>
2018-06-28 08:59:05 +0900

    notice list 서비스 추가

--- src/com/libo/libo/service/member/NoticeService.java
+++ src/com/libo/libo/service/member/NoticeService.java
@@ -1,11 +1,13 @@
 package com.libo.libo.service.member;
 import com.libo.libo.member.Notice;
+import com.libo.libo.member.PlaceFav;
 import com.libo.libo.util.DBConn;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -40,11 +42,33 @@ public class NoticeService {
 	return notice;
 }
 	public List<Notice> getNoticeList() {
+		List<Notice> list = new ArrayList<Notice>();
+		
 		Connection conn = null;
 		PreparedStatement ps =null;
+		ResultSet rs = null;
 		
 		String sql = "SELECT * FROM NOTICE_BOARD";
+		try {
+			conn = DBConn.getConnection();			
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery(sql);
+
+			while (rs.next()) {
+				Notice notice = new Notice();				 
+				notice.setId(rs.getInt("ID"));
+				notice.setSubject(rs.getString("SUBJECT"));
+				notice.setConstant(rs.getString("CONSTANT"));
+				notice.setWriterID(rs.getString("WRITER_ID"));
+				notice.setDate(rs.getString("DUE_DATE"));		 
+				list.add(notice);				 
+			}			
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		} finally {
+			DBConn.close(conn, rs, ps);
+		}
 		
-		return null;
+		return list;
 	}
 }
