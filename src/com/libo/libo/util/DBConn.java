ebbc07616060dbb28c047ee29f0dc4294fca609a
rhqh12 <rhqh12@user-pc>
2018-06-27 15:36:51 +0900

    알람 DTO 변경

--- src/com/libo/libo/util/DBConn.java
+++ src/com/libo/libo/util/DBConn.java
@@ -2,55 +2,94 @@ package com.libo.libo.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DBConn {
 	//http://racoonlotty.tistory.com/41 참고
 	//null reset
-	private static Connection dbConn;	
 	public static Connection getConnection() {
-		if(dbConn == null) {
-			try {
-				
-				//연결 설정
-				String url ="jdbc:oracle:thin:@211.238.142.251:1521:orcl";
-				String user = "c##libo";
-				String pwd = "lifebot123";				
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-				
-				//연결
-				dbConn = DriverManager.getConnection(url, user, pwd);
-				
-				
-			} catch (Exception e) {
-				System.out.println(e.toString());
-			}
+		Connection dbConn = null;
+		try {
+			
+			//연결 설정
+			String url ="jdbc:oracle:thin:@211.238.142.251:1521:orcl";
+			String user = "c##libo";
+			String pwd = "lifebot123";				
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			//연결
+			dbConn = DriverManager.getConnection(url, user, pwd);
+			
+			
+		} catch (Exception e) {
+			System.out.println(e.toString());
 		}
+		
 		return dbConn;		
 	}
 	
-	public static Connection getConnection(String url, String user, String pwd) {
-		if (dbConn == null)	{
-			try {
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-				dbConn = DriverManager.getConnection(url, user, pwd);
-			} catch (Exception e) {
-				System.out.println(e.toString());
-			}
-		}
-		return dbConn;
+	public static void close(Connection conn,  PreparedStatement ps) {
+		close(conn, null, ps);	
 	}
 	
-	public static void close() {
-		if(dbConn != null) {
+	public static void close(Connection conn, ResultSet rs, PreparedStatement ps) {
+		
+		if(rs != null) {
 			try {
-				if(!dbConn.isClosed()) {
-					dbConn.close();		
-				}
+				rs.close();				
 			} catch (Exception e) {
 				System.out.println(e.toString());
 			}
 		}
-		dbConn = null;
+		
+		if(ps != null) {
+			try {
+				ps.close();				
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+		}
+		
+		if(conn != null) {
+			try {
+				conn.close();				
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+		}
 	}
+	
+	public static void close(Connection conn,  Statement st) {
+		close(conn, null, st);
+	}
+	
+	public static void close(Connection conn, ResultSet rs, Statement st) {
+		if(rs != null) {
+			try {
+				rs.close();				
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+		}
+				
+		if(st != null) {
+			try {
+				st.close();				
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+		}
+		
+		if(conn != null) {
+			try {
+				conn.close();				
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+		}	
+	}
+	
 
 }
