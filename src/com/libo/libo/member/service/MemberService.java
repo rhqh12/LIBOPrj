878ea3850eb4df9c8593a0175ed94f40b0e18d4b
user <user@user-pc>
2018-06-27 09:01:55 +0900

    회원 서비스 변경

--- src/com/libo/libo/member/service/MemberService.java
+++ src/com/libo/libo/member/service/MemberService.java
@@ -3,22 +3,22 @@ import com.libo.libo.member.Member;
 
 public class MemberService {
 	//회원가입
-	public Member joinMember() {
+	public Member join() {
 		return null;
 	}	
 	
 	//회원정보 수정
-	public void updateMember(Member member) {
+	public void update(Member member) {
 		
 	}
 	
 	//로그인
-	public boolean loginMember(Member member) {
+	public boolean login(Member member) {
 		return false;		
 	}
 	
 	//로그아웃
-	public void logoutMember(Member member) {
+	public void logout(Member member) {
 		
 	}
 }
