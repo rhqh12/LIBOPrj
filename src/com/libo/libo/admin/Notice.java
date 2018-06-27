8468d54d54feea2a611d338291c99a67783a0860
user <user@user-pc>
2018-06-27 10:32:29 +0900

    공지사항 이름 변경

--- src/com/libo/libo/admin/Notice.java
+++ src/com/libo/libo/admin/Notice.java
@@ -1,5 +1,10 @@
 package com.libo.libo.admin;
 
 public class Notice {
-
+	private int id;
+	
+	//생성자 생성
+	public Notice() {
+		
+	}	
 }
--- src/com/libo/libo/service/admin/NoticeService.java
+++ src/com/libo/libo/service/admin/NoticeService.java
@@ -3,6 +3,16 @@ package com.libo.libo.service.admin;
 import com.libo.libo.admin.Notice;
 
 public class NoticeService {
+	
+	public static void main(String[] args) {
+		/*Notice Service service = new NoticeService();
+		 * Notice notice = new Notice("aa", "newlec");
+		 * ;
+		notice.setTitle("aaa");
+		service.insert*/
+	}
+	
+	
 	public void insertNotice(Notice notice) {
 		
 	}
