f6cdc3e171c923e96af3c7bf603d930872b8d71c
koby <koby@192.168.0.101>
2018-07-24 18:04:01 +0900

    Keyword entity

---
+++ src/com/libo/web/entity/Keyword.java
@@ -0,0 +1,31 @@
+package com.libo.web.entity;
+
+public class Keyword {
+	String name;
+	int id;
+	
+	public Keyword() {
+		
+	}
+	public Keyword(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+
+}
